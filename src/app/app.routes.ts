67db52c9a9186c4de26030f65f3ee8ee17c56787
kishan savaliya <kishan1054@gmail.com>
2024-01-03 21:09:58 +0530

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'seller',
+    component: SellerAuthComponent,
+  },
+  {
+    path: 'login',
+    component: SellerAuthComponent,
+  },
+  {
+    path: 'cart',
+    component: SellerAuthComponent,
+  },
+];
