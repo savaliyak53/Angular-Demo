5d4a7e4b8bfd4b04dd1a588bf5c37e6aedb6ff77
kishan savaliya <kishan1054@gmail.com>
2024-01-04 11:21:06 +0530

    Add seller sign up form

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './seller-auth.component.html',
-  styleUrl: './seller-auth.component.css'
+  styleUrl: './seller-auth.component.css',
 })
-export class SellerAuthComponent {
-
+export class SellerAuthComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+  signUp(data: object): void {
+    console.log(data);
+  }
 }
